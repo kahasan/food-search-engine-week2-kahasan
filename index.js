f40dd9a2217c779a5471c0d77fbc545e4e93f7df
kahasan <kahraman.h@outlook.com>
2021-07-15 19:55:19 +0300

    Add airtable config for api

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-console.log('test')
+import 'regenerator-runtime/runtime';
+import Fuse from 'fuse.js';
+
+import dotenv from 'dotenv';
+dotenv.config();
+
+import { getTable } from './lib/airtable';
+
+const cards = document.getElementById('js-cards');
+const title = document.createElement('h1');
+
+async function getData(searchKey) {
+  await getTable('Meals').then((res) => {
+    res.forEach((element) => {
+      const fuse = new Fuse([element], {
+        keys: ['strMeal'],
+      });
+      const result = fuse.search(searchKey);
+      if (result.length < 0) return;
+      result.length > 0 && (title.innerText = result[0].item.strMeal);
+      cards.appendChild(title);
+    });
+  });
+}
+
+getData('cor');
