ef5199a5911b37951c3b13fcfd1c7f49000b2a04
kahasan <kahraman.h@outlook.com>
2021-07-17 12:26:42 +0300

    Code refactoring

--- index.js
+++ index.js
@@ -6,66 +6,82 @@ dotenv.config();
 
 import { getTable } from './lib/airtable';
 
-const cards = document.getElementById('js-cards');
+class SearhFood {
+  constructor(mealsObj) {
+    this.store = {
+      meals: mealsObj.map(({ item }) => ({ ...item, isFavorited: false })),
+    };
+    this.cards = document.getElementById('js-cards');
+    this.input = document.getElementById('search');
 
-const input = document.getElementById('search');
+    this.showCard();
 
-function createCard(item) {
-  const card = document.createElement('div');
-  const mealTitle = document.createElement('h3');
-  const mealImg = document.createElement('img');
-  const mealSide = document.createElement('div');
+    this.input.addEventListener('input', (e) => {
+      console.log('triggered');
+      function debounce(func, timeout = 300) {
+        let timer;
+        return (...args) => {
+          clearTimeout(timer);
+          timer = setTimeout(() => {
+            func.apply(this, args);
+          }, timeout);
+        };
+      }
+      const processChange = debounce(() => this.searchItem(e.target.value));
+      processChange();
+    });
+  }
 
-  mealTitle.innerText = item.strMeal;
-  mealImg.src = item.strMealThumb;
-  mealSide.innerText = item.strArea;
-  card.classList.add('card');
-
-  card.appendChild(mealTitle);
-  card.appendChild(mealImg);
-  card.appendChild(mealSide);
-  cards.appendChild(card);
-}
-
-async function getData(searchKey) {
-  await getTable('Meals').then((res) => {
+  searchItem(value) {
+    if (!value) return;
     const options = {
       keys: ['strMeal'],
     };
-    const fuse = new Fuse(res, options);
-    const result = searchKey && fuse.search(searchKey);
-    console.log('result', result);
 
-    cards.innerHTML = '';
+    const fuse = new Fuse(this.store.meals, options);
+    const searhResponse = value && fuse.search(value);
+    const newResponse = searhResponse.map(({ item }) => ({ ...item }));
+    this.store.meals = newResponse;
+    this.showCard();
+  }
 
-    !searchKey &&
-      res.map((item) => {
-        createCard(item);
-      });
+  showCard() {
+    this.cards.innerHTML = '';
 
-    result &&
-      result.map(({ item }) => {
-        createCard(item);
-      });
+    const mealsData = this.store.meals.map((meal) => {
+      this.createCard(meal);
+    });
+    return mealsData;
+  }
+
+  createCard(item) {
+    const { strMeal, strMealThumb, strArea, isFavorited } = item;
+
+    const card = document.createElement('div');
+    const mealTitle = document.createElement('h3');
+    const mealImg = document.createElement('img');
+    const mealSide = document.createElement('div');
+    const favBtn = document.createElement('img');
+
+    card.classList.add('card');
+    favBtn.classList.add('fav-btn');
+
+    favBtn.src = isFavorited ? 'liked.svg' : 'like.svg';
+    mealTitle.innerText = strMeal;
+    mealImg.src = strMealThumb;
+    mealSide.innerText = strArea;
+
+    card.appendChild(mealTitle);
+    card.appendChild(mealImg);
+    card.appendChild(mealSide);
+    card.appendChild(favBtn);
+    this.cards.appendChild(card);
+  }
+}
+
+(async function getData() {
+  await getTable('Meals').then((res) => {
+    const response = res.map((item) => ({ item: { ...item } }));
+    new SearhFood(response);
   });
-}
-
-getData();
-
-function debounce(func, timeout = 300) {
-  let timer;
-  return (...args) => {
-    clearTimeout(timer);
-    timer = setTimeout(() => {
-      func.apply(this, args);
-    }, timeout);
-  };
-}
-
-input.addEventListener('input', (e) => {
-  const value = e.target.value;
-
-  const processChange = debounce(() => getData(value));
-
-  processChange();
-});
+})();
