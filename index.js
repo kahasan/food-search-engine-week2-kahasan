900676785c7d1d9d23486a84d0c4107ea6efe8b2
kahasan <kahraman.h@outlook.com>
2021-07-17 06:00:51 +0300

    Edit search func and add debouncing

--- index.js
+++ index.js
@@ -7,20 +7,65 @@ dotenv.config();
 import { getTable } from './lib/airtable';
 
 const cards = document.getElementById('js-cards');
-const title = document.createElement('h1');
+
+const input = document.getElementById('search');
+
+function createCard(item) {
+  const card = document.createElement('div');
+  const mealTitle = document.createElement('h3');
+  const mealImg = document.createElement('img');
+  const mealSide = document.createElement('div');
+
+  mealTitle.innerText = item.strMeal;
+  mealImg.src = item.strMealThumb;
+  mealSide.innerText = item.strArea;
+  card.classList.add('card');
+
+  card.appendChild(mealTitle);
+  card.appendChild(mealImg);
+  card.appendChild(mealSide);
+  cards.appendChild(card);
+}
 
 async function getData(searchKey) {
   await getTable('Meals').then((res) => {
-    res.forEach((element) => {
-      const fuse = new Fuse([element], {
-        keys: ['strMeal'],
+    const options = {
+      keys: ['strMeal'],
+    };
+    const fuse = new Fuse(res, options);
+    const result = searchKey && fuse.search(searchKey);
+    console.log('result', result);
+
+    cards.innerHTML = '';
+
+    !searchKey &&
+      res.map((item) => {
+        createCard(item);
       });
-      const result = fuse.search(searchKey);
-      if (result.length < 0) return;
-      result.length > 0 && (title.innerText = result[0].item.strMeal);
-      cards.appendChild(title);
-    });
+
+    result &&
+      result.map(({ item }) => {
+        createCard(item);
+      });
   });
 }
 
-getData('cor');
+getData();
+
+function debounce(func, timeout = 300) {
+  let timer;
+  return (...args) => {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      func.apply(this, args);
+    }, timeout);
+  };
+}
+
+input.addEventListener('input', (e) => {
+  const value = e.target.value;
+
+  const processChange = debounce(() => getData(value));
+
+  processChange();
+});
